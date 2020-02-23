430418ebf041c4f0e2e6a6be54b076108e89a73f
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-22 22:27:05 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function generateReducer() {
+    let actionMap = {}
+    let initialState
+
+    function setInitialState(initialState) {
+        initialState = initialState
+    }
+
+    function addAction(action, callback) {
+        switch(typeof action) {
+            case 'object':
+                actionMap[action.type] = callback
+                break
+            case 'function':
+                actionMap[action()] = callback
+                break
+            case 'string':
+                actionMap[action] = callback
+                break
+            default:
+                throw new Error("Action must be either an object, function or a string")
+        }
+    }
+
+    function buildReducer() {
+        return function(state = initialState, action) {
+            processor = actionMap[action.type]
+            if (processor) {
+                return processor(state, action)
+            }
+
+            return state
+        }
+    }
+
+    return {setInitialState, addAction, buildReducer}
+}
+
+module.exports = generateReducer()
---
+++ test/test.js
@@ -0,0 +1,59 @@
+let expect = require('expect.js')
+
+let builder = require('../index')
+
+describe('generateReducer', function() {
+    describe('functions', function() {
+        it('should return an object', function() {
+            expect(typeof builder).to.be('object')
+        })
+
+        it('should have a key of setInitialState', function() {
+            expect(builder.setInitialState).to.not.be(undefined)
+
+            expect(typeof builder.setInitialState).to.be('function')
+        })
+
+        it('should have a key of addAction', function() {
+            expect(builder.addAction).to.not.be(undefined)
+            expect(typeof builder.addAction).to.be('function')
+        })
+
+        it('should have a key of buildReducer', function() {
+            expect(builder.buildReducer).to.not.be(undefined)
+            expect(typeof builder.buildReducer).to.be('function')
+        })
+    })
+
+    describe('setInitialState', function() {
+        it('should return undefined', function() {
+            expect(builder.setInitialState({})).to.be(undefined)
+        })
+    })
+
+    describe('addAction', function() {
+        it('should return undefined', function() {
+            expect(builder.addAction({}, ()=>{})).to.be(undefined)
+        })
+
+        it('should throw an error if given improper action type argument', function() {
+            expect(builder.addAction.bind(this, 5, ()=>{})).to.throwError()
+        })
+    })
+
+    describe('buildReducer', function() {
+        it('should return a function and should return initial state plus "!"', function() {
+            const initialState = {test: "Hello World"}
+            builder.setInitialState(initialState)
+            builder.addAction('DEMO', function(state, action) {
+                return {
+                    test: state.test + "!"
+                }
+            })
+            const reducer = builder.buildReducer()
+            expect(typeof reducer).to.be('function')
+
+            expect(reducer(initialState, {type: 'DEMO'}).test).to.be("Hello World!")
+        })
+    })
+})
